d64b3480eca53030989016f56aa7180cc16c9e3f
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-25 00:56:15 +0530

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <Main/>
+       <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind + Vite + React works!</h1>
+    </div>
     </>
   )
 }
