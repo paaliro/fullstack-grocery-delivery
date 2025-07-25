d41f6897779d2db4eef555f68022393ea8ebe03d
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-25 23:27:47 +0530

    start working.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ function App() {
   return (
     <>
       <Main/>
-       <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind + Vite + React works!</h1>
-    </div>
     </>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,6 @@
 const Main = () => {
     return <div>
-        <p className="text-center text-2xl">Now, it's working as per expectation.</p>
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Vite + React!</h1>
-        </div>
+
     </div>
 }
 
