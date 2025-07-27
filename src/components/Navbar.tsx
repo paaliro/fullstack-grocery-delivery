205e303a568caf1bcf3d159cdf48cd516fd0ebab
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-27 23:28:45 +0530

    working on navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
-        <div className="w-full border-2 border-amber-800 py-8">
-            <div className="flex items-center justify-center border-2 border-sky-400 bg-[#fbf4e4]">
-                <img src="./images/FreshBasket/freshBasket.png" alt="brand-logo" />
+        <div className="w-full border-2 border-amber-800">
+            <div className="flex items-center justify-between border-2 border-sky-400 bg-[#fbf4e4] px-22">
+                <img src="./images/FreshBasket/freshBasket.png" alt="brand-logo" width={100} height={80}/>
                 <p>Navbar</p>
             </div>
         </div>
