b2a2ba44ccdd677b06acdd891812aeef4749825b
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-28 23:53:23 +0530

    little changes are done & project deployed to vercel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = () => {
                     <img src="./images/menu_icon.svg" alt="menu-icon" className="w-[24px] cursor-pointer" />
                 </div>
                 <div className="hidden md:flex justify-around items-center w-[60%]">
-                    <p className="cursor-pointer border-1 border-gray-400 px-3 rounded-3xl text-sm text-gray-400">Seller Dashboard</p>
+                    <p className="cursor-pointer border-1 border-gray-400 px-3 rounded-3xl text-sm text-gray-500">Seller Dashboard</p>
                     <p className="cursor-pointer">Home</p>
                     <p className="cursor-pointer">All Product</p>
                     <div className="flex border-[1.5px] border-gray-400 rounded-3xl px-4">
-                        <input type="text" placeholder="Search" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" />
-                        <img src="./images/search_icon.svg" alt="" />
+                        <input type="text" placeholder="Search Products" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500 text-sm" />
+                        <img src="./images/search_icon.svg" alt="search-icon" />
                     </div>
                     <div className="relative inline-block">
                         <button className="absolute -top-1.5 right-0 left-5 bg-[#fdb81d] text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">0</button>
