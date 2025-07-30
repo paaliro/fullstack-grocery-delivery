4a467fdcf4eae406f5a3b10048edc0d9bd966dd2
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-07-30 22:24:10 +0530

    start woring again.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,8 +2,10 @@ const Main = () => {
     return (
         < div >
             {/* <p>hello</p> */}
+            <img src="/images/main_banner_bg.png" alt="banner-image" className="grayscale hover:grayscale-0"/>
         </div >
     )
+    
 }
 
 export default Main;
