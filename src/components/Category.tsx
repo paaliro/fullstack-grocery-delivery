6e01bddf22d9263e1f98078567616dd913f931cb
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-01 22:13:40 +0530

    ignore

---
+++ src/components/Category.tsx
@@ -0,0 +1,7 @@
+const Category = () => {
+    return <div className="px-4 md:px-25">
+        <h1 className="text-2xl md:text-3xl font-medium text-[#364152]">Categories</h1>
+    </div>
+}
+
+export default Category;
