2a494d3721fde371c96a32f432dd31c62e330ee0
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-05 21:53:18 +0530

    ignore

--- src/components/BestSellers.tsx
+++ src/components/BestSellers.tsx
@@ -1,7 +1,7 @@
 const BestSellers = () => {
     return <div className="px-6 md:px-24 mt-14 md:mt-18 mb-4 md:mb-5">
         <div className="">
-            <h1 className="text-2xl md:text-3xl font-medium text-[#364152]">Best Sellers </h1>
+            <h1 className="text-2xl md:text-3xl font-medium text-[#364152]">Best Sellers</h1>
         </div>
     </div>
 }
