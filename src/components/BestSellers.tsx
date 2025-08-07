5f124212c653f391195b0b822c94b560e953bb9e
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-07 23:50:38 +0530

    first box is complete & now working on another ones.

--- src/components/BestSellers.tsx
+++ src/components/BestSellers.tsx
@@ -1,14 +1,14 @@
 import { stars } from "../assets/helper";
 
 const BestSellers = () => {
-    return <section className="px-6 md:px-24 mt-14 md:mt-18 mb-4 md:mb-5 py-20">
+    return <section className="px-6 md:px-24 mt-14 md:mt-16 mb-4 md:mb-5">
         <div className="">
             <h1 className="text-2xl md:text-3xl font-medium text-[#364152]">Best Sellers</h1>
 
             {/* <div className="border-1 border-[#e1e3e6] rounded-md grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"> */}
-            <div className="border-1 border-[#e1e3e6] rounded-md my-10 w-fit">
+            <div className="border-1 border-[#e1e3e6] rounded-md my-6 w-fit">
                 <div className="w-fit px-4 py-2">
-                    <img src="/images/best-sellers/bakery_image.png" alt="potato-img" className="max-w-40 cursor-pointer transform transition-transform duration-300 hover:scale-105" />
+                    <img src="/images/best-sellers/bakery_image.png" alt="potato-img" className="max-w-40 md:max-w-45 cursor-pointer transform transition-transform duration-300 hover:scale-105" />
                     <div className="">
                         <div>
                             <p className="text-gray-500/60 text-sm">Bakery</p>
@@ -24,7 +24,7 @@ const BestSellers = () => {
                                 <p className="text-xl font-semibold text-[#feca65]">₹35
                                     <span className="text-sm text-gray-500/60 line-through px-1">₹40</span>
                                 </p>
-                                <button className="flex gap-2 border-1 border-[#feca65] px-2 py-0.5 rounded-md bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
+                                <button className="flex gap-2 border-1 border-[#feca65] px-2 md:px-3 py-0.5 rounded-md bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
                                     <img src="/images/updated-svg/cart_icon.svg" alt="cart-icon" className="w-4" />
                                     Add
                                 </button>
