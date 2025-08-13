7c86d76e6bbc0199ecea603b4f57ddd36596508b
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-13 22:03:17 +0530

    working on final touchs at U.I.

--- src/components/NeverMiss.tsx
+++ src/components/NeverMiss.tsx
@@ -5,7 +5,7 @@ const NeverMiss = () => {
             <p className="text-gray-500/80 text-sm md:text-lg mt-2 text-center">Subscribe to get the latest offers, new arrivals, and exclusive discounts</p>
             <div className="flex mt-12 mb-10 md:w-[55%]"> 
                 <input type="text" name="subscribe-input" id="subscribe-input" placeholder="Enter your email id" className="border border-gray-500/30 rounded-l-md outline-none w-full px-3 text-gray-500 py-3" />
-                <button className="md:px-12 px-8 text-white bg-[#fdb81d] rounded-r-md py-3 text-md hover:bg-[#e5a91a] transition-colors">Subscribe</button>
+                <button className="md:px-12 px-8 text-white bg-[#fdb81d] rounded-r-md py-3 text-md hover:bg-[#e5a91a] transition-colors">Subscribe</button> 
             </div>
         </div>
     </section>
