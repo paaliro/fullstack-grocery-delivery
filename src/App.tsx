6cb6533242c2c38d958d50be73853553a0fced74
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-17 23:01:47 +0530

    changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-// import Footer from "./components/Footer";
-// import Main from "./components/Main";
-// import Navbar from "./components/Navbar";
-// function App() {
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <Main />
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-// import Home from "./pages/Main";
-import Products from "./pages/Products"; // <-- your products page
+import Products from "./pages/Products";
 import Main from "./components/Main";
 
 function App() {
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,6 @@
 const Products = () => {
     return <section className="px-6 md:px-24">
-        <h1 className="text-2xl md:text-3xl text-[#364152] mt-10">ALL PRODUCTS</h1>
+        <h1 className="text-xl md:text-3xl text-[#364152] mt-10">ALL PRODUCTS</h1>
 
     </section>
 }
