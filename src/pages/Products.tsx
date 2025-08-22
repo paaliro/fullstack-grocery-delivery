ea9305ee1af90b0a9ec70bf513496674e25d6e8e
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-22 23:36:37 +0530

    some changes are running.

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -9,7 +9,7 @@ const Products = () => {
                 <div key={index} className="border-1 border-[#e1e3e6] rounded-md">
                     <div className="px-4 md:px-4 py-2">
                         <div className="flex justify-center">
-                            <img src={data.imgPath} alt="potato-img" className="max-w-28 md:max-w-36 cursor-pointer transform transition-transform duration-300 hover:scale-105" />
+                            <img src={data.imgPath} alt="items-img" className="max-w-28 md:max-w-36 cursor-pointer transform transition-transform duration-300 hover:scale-105" />
                         </div>
                         <div>
                             <div>
@@ -19,15 +19,15 @@ const Products = () => {
                                     {stars.map((src, index) => (
                                         <img key={index} src={src} alt="star-img" className="w-3" />
                                     ))}
-                                    <p className="text-gray-500/60 text-sm">(4)</p>
+                                    <p className="text-gray-500/60 text-xs">(4)</p>
                                 </div>
 
                                 <div className="flex justify-between items-baseline pt-3">
                                     <p className="text-sm md:text-xl lg:2xl font-semibold text-[#feca65]">₹{data.actualPrice}
                                         <span className="text-xs md:text-sm text-gray-500/60 line-through px-1">₹{data.oldPrice}</span>
                                     </p>
-                                    <button className="flex gap-0.5 md:gap-2 border-1 text-xs md:text-lg border-[#feca65] px-1 md:px-3 py-0.5 rounded-md bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
-                                        <img src="/images/updated-svg/cart_icon.svg" alt="cart-icon" className="w-3 md:w-4" />
+                                    <button className="flex gap-0.5 md:gap-2 border-1 text-xs md:text-lg border-[#feca65] px-1 md:px-3 py-0.5 rounded-xs md:rounded-sm bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
+                                        <img src="/images/updated-svg/cart_icon.svg" alt="cart-icon" className="w-2.5 md:w-4" />
                                         Add
                                     </button>
                                 </div>
