949a8e1553de9f11f4b8deb40a7b342c6c1e07b8
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-27 23:20:45 +0530

    Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -13,7 +13,7 @@ const Products = () => {
                     <div className="px-4 md:px-4 py-2">
                         <div className="flex justify-center">
                             <img src={data.imgPath} alt="items-img" className="max-w-28 md:max-w-38 h-30 md:h-fit cursor-pointer transform transition-transform duration-300 hover:scale-105" />
-                        </div>
+                        </div> 
                         <div>
                             <div>
                                 <p className="text-gray-500/60 text-xs md:text-sm">{data.categoryName}</p>
