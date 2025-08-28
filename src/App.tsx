6b1c2d7abc1d5c78867bb25494d5e45e4e0ae94f
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-08-28 23:35:41 +0530

    little chnages

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/vegetables" element={<Products />} />
         {/* Add more pages here */}
       </Routes>
 
