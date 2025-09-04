3fd7affe9ec53a5225275abe0583d1d46e6f71d4
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-05 00:42:39 +0530

    fix bug in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@
 
 // export default App;
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
