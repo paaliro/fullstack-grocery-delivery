1f9a89ea19d6ebf2be0b0611464b993770475a33
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-05 00:43:26 +0530

    remove unecassary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Footer from "./components/Footer";
-// import Navbar from "./components/Navbar";
-// import Products from "./pages/Products";
-// import Main from "./components/Main";
-// import Vegetables from "./pages/Vegetables";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Main />} />
-//         <Route path="/products" element={<Products />} />
-//         <Route path="/vegetables" element={<Vegetables />} />
-//         {/* Add more pages here */}
-//       </Routes>
-
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
