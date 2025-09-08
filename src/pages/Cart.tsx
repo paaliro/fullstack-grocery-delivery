e52f2780efffd416a5343f7787b043440c3983c8
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-08 23:04:25 +0530

    done

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,7 +2,7 @@ const Cart = () => {
     return <section className="px-4 md:px-22">
         <div className="flex gap-0.5 py-10">
             <p className="text-3xl font-medium text-[#364152]">Shopping Cart</p>
-            {/* <span className="pt-2 text-[#364152]">0 items</span> */}
+            <span className="pt-2 text-[#364152]">0 items</span>
         </div>
     </section>
 }
