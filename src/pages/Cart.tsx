a8c06aff1d4aa9f9b0110ae37f24da882ecf29fd
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-08 23:03:34 +0530

    making cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,9 @@
 const Cart = () => {
     return <section className="px-4 md:px-22">
 
-        <div className="flex">
-            <h1 className="text-3xl font-medium mb-6">Shopping Cart</h1>
-            <span>0 items</span>
+        <div className="flex gap-0.5 py-20">
+            <p className="text-3xl font-medium">Shopping Cart</p>
+            <span className="pt-2">0 items</span>
         </div>
     </section>
 }
