29bc0d8047f46aed28446a2d0ad6a8155352f39c
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-08 23:04:10 +0530

    making tcontent done.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,8 @@
 const Cart = () => {
     return <section className="px-4 md:px-22">
-
-        <div className="flex gap-0.5 py-20">
-            <p className="text-3xl font-medium">Shopping Cart</p>
-            <span className="pt-2">0 items</span>
+        <div className="flex gap-0.5 py-10">
+            <p className="text-3xl font-medium text-[#364152]">Shopping Cart</p>
+            {/* <span className="pt-2 text-[#364152]">0 items</span> */}
         </div>
     </section>
 }
