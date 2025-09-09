20474ba7b1eaea5d055f9b238b9c4ae8f5fd9856
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-09 22:57:49 +0530

    Cart.tsx under workng.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,21 @@
 const Cart = () => {
     return <section className="px-4 md:px-22">
-        <div className="flex gap-0.5 py-10">
-            <p className="text-3xl font-medium text-primary">Shopping Cart</p>
-            <span className="pt-2 text-secondary font-medium">0 items</span>
+        <div className="flex flex-col md:flex-row mt-16">
+            <div className="max-w-4xl border-1 border-emerald-500">
+                <p className="text-3xl font-medium text-primary">Shopping Cart
+                    <span className="text-secondary font-medium text-sm">0 items</span>
+                </p>
+                <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
+                    <p>Product Details</p>
+                    <p>Subtotal</p>
+                    <p>Action</p>
+                </div>
+            </div>
+            <div className="border-1 border-gray-500">
+                <p className="text-3xl font-medium text-primary">Order Summary</p>
+            </div>
         </div>
+
     </section>
 }
 
