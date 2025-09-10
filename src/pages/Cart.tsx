94b7e4ba53c2a639a768fe94ee5239c34f9477b4
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-10 23:31:02 +0530

    adjustments are working.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,17 +1,17 @@
 const Cart = () => {
     return <section className="px-4 md:px-22">
         <div className="flex flex-col md:flex-row mt-16">
-            <div className="max-w-4xl w-8/12 border-1 border-emerald-500">
-                <p className="text-3xl font-medium text-primary">Shopping Cart
-                    <span className="text-secondary font-medium text-sm">0 items</span>
+            <div className="max-w-4xl md:w-8/12 w-full border-1 border-emerald-500">
+                <p className="px-4 text-3xl font-medium text-primary">Shopping Cart
+                    <span className="px-1 text-secondary font-medium text-sm">0 items</span>
                 </p>
-                <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
-                    <p>Product Details</p>
-                    <p>Subtotal</p>
-                    <p>Action</p>
+                <div className="px-4 pt-6 grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
+                    <p className="text-left">Product Details</p>
+                    <p className="text-center">Subtotal</p>
+                    <p className="text-center">Action</p>
                 </div>
             </div>
-            <div className="border-1 w-4/12 border-gray-500">
+            <div className="border-1 md:w-4/12 w-full border-gray-500">
                 <p className="text-3xl font-medium text-primary">Order Summary</p>
             </div>
         </div>
