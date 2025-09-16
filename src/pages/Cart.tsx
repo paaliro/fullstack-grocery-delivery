e3b1b021d0abce130fd53e2fb640f11ae853b0b2
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-16 22:12:00 +0530

    started working Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,15 +1,19 @@
 const Cart = () => {
-    return <section className="px-4 md:px-22 my-20">
+    return <section className="px-4 md:px-22 my-15 md:my-20">
         <div className="flex flex-col md:flex-row">
             <div className="max-w-4xl md:w-8/12 w-full">
-                <p className="px-4 text-3xl font-medium text-primary">Shopping Cart
-                    <span className="px-1 text-secondary font-medium text-sm">0 items</span>
+                <p className="text-3xl font-medium text-primary">Shopping Cart
+                    <span className="px-0.5 text-secondary font-medium text-sm">0 items</span>
                 </p>
-                <div className="px-4 pt-6 grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
+                <div className="pt-6 grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
                     <p className="text-left">Product Details</p>
                     <p className="text-center">Subtotal</p>
                     <p className="text-center">Action</p>
                 </div>
+                <button className="text-secondary">
+                    <img alt="arrow-img" src="/images/black_arrow_icon.svg"></img>
+                    Continue Shopping
+                </button>
             </div>
             <div className="md:w-4/12 w-full bg-[#f3f4f6] px-3 md:px-5 mr-3 rounded-sm">
                 <p className="text-xl md:text-2xl font-medium text-primary pt-5">Order Summary</p>
