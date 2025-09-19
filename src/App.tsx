e09180fd49b7efdf8d21f9e8ebb7a8f23e1a537a
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-19 23:26:28 +0530

    scrolltotop bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Products from "./pages/Products";
 import Main from "./components/Main";
 import CategoryPage from "./pages/CategoryPage";
 import Cart from "./pages/Cart";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
 
       <Routes>
