7e2cada9fbad714e00bf4eced7e320b126db4e4c
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-09-21 20:56:06 +0530

    PageNotFound.tsx completed.

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center text-sm h-[400px]">
-            <p className="font-bold md:text-4xl text-3xl text-secondary">404</p>
+        <div className="flex flex-col items-center justify-center text-sm h-[400px] my-20">
+            <p className="font-semibold md:text-4xl text-3xl text-secondary mb-2">404</p>
             <h2 className="md:text-5xl text-3xl font-semibold text-primary">Page Not Found</h2>
             <p className="text-base mt-4 text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="flex items-center gap-4 mt-6">
@@ -12,7 +12,6 @@ const PageNotFound = () => {
                         Go back home
                     </button>
                 </Link>
-
             </div>
         </div>
     );
