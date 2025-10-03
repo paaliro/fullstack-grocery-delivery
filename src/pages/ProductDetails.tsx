1c4a3b0eea18ac721aa167fc92c1b4b6b17fa2d9
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-10-03 23:57:33 +0530

    ProductDetails.tsx alignments

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -33,7 +33,7 @@ const ProductDetails = () => {
                     <span className="text-indigo-500"> {product.name}</span>
                 </p>
 
-                <div className="flex flex-col md:flex-row gap-16 mt-4">
+                <div className="flex flex-col md:flex-row gap-16 mt-4"> 
                     <div className="flex gap-3">
                         <div className="flex flex-col gap-3">
                             {product.images.map((image, index) => (
