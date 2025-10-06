ae815a576ebee4d94f33914fe736066771cc2095
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-10-06 23:15:27 +0530

    working on

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -49,7 +49,7 @@ const ProductDetails = () => {
                     </div>
 
                     <div className="text-sm w-full md:w-1/2">
-                        <h1 className="text-3xl font-medium">{product.name}</h1>
+                        <h1 className="text-3xl font-medium">{product.name}</h1> 
 
                         <div className="flex items-center gap-0.5 mt-1">
                             {Array(5).fill('').map((_, i) => (
