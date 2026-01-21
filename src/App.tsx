2e02457e09b6d75bd4aa3b2458f3b49ff6579036
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2026-01-21 22:54:34 +0530

    Fixed '#' Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Products from "./pages/AllProducts";
