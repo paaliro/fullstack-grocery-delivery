eedfffb9c4bddb5bb5fd3c93ec25893820883449
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2026-04-13 22:49:46 +0530

    Working on BestSeller.tsx

--- src/components/BestSellers.tsx
+++ src/components/BestSellers.tsx
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 import { allProductsContent, stars } from "../assets/data";
 
 const BestSellers = () => {
-    // shuffle + take first 8 products
-    const randomProducts = [...allProductsContent]
-        .sort(() => Math.random() - 0.5) // shuffle
-        .slice(0, 5); // take 5
+    // shuffle + take first 5 products
+    const randomProducts = [...allProductsContent].sort(() => Math.random() - 0.5).slice(0, 5);
 
     return (
         <section className="px-6 md:px-24 mt-14 md:mt-16 mb-4 md:mb-5">
@@ -18,20 +16,14 @@ const BestSellers = () => {
                             <div className="px-4 md:px-4 py-2">
                                 <div className="flex justify-center">
                                     <Link key={data.id} to={`/products/${data.categoryName.toLowerCase()}/${data.id}`}>
-                                        <img
-                                            src={data.imgPath}
-                                            alt={data.productName}
-                                            className="max-w-28 md:max-w-36 cursor-pointer transform transition-transform duration-300 hover:scale-105"
-                                        />
+                                        <img src={data.imgPath} alt={data.productName} className="max-w-28 md:max-w-36 cursor-pointer transform transition-transform duration-300 hover:scale-105" />
                                     </Link>
                                 </div>
                                 <div>
                                     <p className="text-gray-500/60 text-xs md:text-sm">{data.categoryName}</p>
                                     <p className="font-medium text-sm md:text-lg">{data.productName}</p>
                                     <div className="flex gap-0.5">
-                                        {stars.map((src, index) => (
-                                            <img key={index} src={src} alt="star-img" className="w-3" />
-                                        ))}
+                                        {stars.map((src, index) => (<img key={index} src={src} alt="star-img" className="w-3" />))}
                                         <p className="text-gray-500/60 text-sm">(4)</p>
                                     </div>
 
@@ -42,7 +34,7 @@ const BestSellers = () => {
                                                 ₹{data.oldPrice}
                                             </span>
                                         </p>
-                                        <button className="flex gap-0.5 md:gap-2 border-1 text-xs md:text-lg border-[#feca65] px-1 md:px-3 py-0.5 rounded-md bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
+                                        <button onClick={() => { console.log("add to cart:", data) }} className="flex gap-0.5 md:gap-2 border-1 text-xs md:text-lg border-[#feca65] px-1 md:px-3 py-0.5 rounded-md bg-[#fff7e7] text-[#feca65] font-normal cursor-pointer">
                                             <img src="/images/updated-svg/cart_icon.svg" alt="cart-icon" className="w-3 md:w-4" />
                                             Add
                                         </button>
